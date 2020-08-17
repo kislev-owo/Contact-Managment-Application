986717567b8dcb717a9f1d461b25ce91286d995c
kislev-owo <kislev_083@hotmail.com>
2020-08-17 06:58:27 +0000

    metodo get y post completados

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,80 @@
-const getState = ({ getStore, getActions, setStore }) => {
+import PropTypes from "prop-types";
+
+const getState = ({ getStore, setStore, getActions }) => {
 	return {
 		store: {
-			demo: [
+			agenda: [
 				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
+					agenda_slug: "kislev-owo",
+					name: "",
+					email: "",
+					phone: "",
+					address: ""
 				}
 			]
+			//Your data structures, A.K.A Entities
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			cargarContactos: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/kislev-owo")
+					.then(response => response.json())
+					.then(data => {
+						setStore({ contacts: data });
+					});
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			añadirContactos: arrayContacto => {
+				console.log("input", arrayContacto);
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "POST",
+					body: JSON.stringify(arrayContacto),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(response => response.json())
+					.then(data => {
+						console.log("output", data);
+						alert("nuevo contacto añadido");
+						getActions().cargarContactos();
+						// props.history.push("/Contacts")
+					})
+					.catch(error => console.error("Error:", error));
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			eliminarContactos: contact_id => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/" + contact_id, {
+					method: "DELETE"
+				})
+					.then(response => response.json())
+					.then(data => {
+						console.log("delete", data);
+						getActions().añadirContactos();
+						alert("Contact Delete, OK!");
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+						// props.history.push("/Contacts")
+					})
+					.catch(error => console.error("Error:", error));
+			},
+			editarContactos: (agenda, contact_id) => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/" + contact_id, {
+					method: "PUT",
+					body: JSON.stringify(agenda),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(response => response.json())
+					.then(data => {
+						alert("Success:", JSON.stringify(data));
+						//1.-para acceder a los metodos primera forma
+						//const actions = getActions();
+						//actions.loadContacts();
+						//2.- segunda forma
+						getActions().añadirContactos();
+					})
+					.catch(error => console.log("Error:", error));
 			}
+			//(Arrow) Functions that update the Store
+			// Remember to use the scope: scope.state.store & scope.setState()
 		}
 	};
 };
---
+++ src/js/views/AddContact.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+
+export const AddContact = () => {
+	const [newName, setnewName] = useState();
+	const [newEmail, setnewEmail] = useState();
+	const [newPhone, setnewPhone] = useState();
+	const [newAddress, setnewAddres] = useState();
+
+	const [arrayContacto, setarrayContacto] = useState();
+	const { actions } = useContext(Context);
+
+	useEffect(
+		() => {
+			setarrayContacto({
+				//parentesis y llaves para trabajar con props
+				agenda_slug: "kislev-owo",
+				full_name: newName,
+				address: newAddress,
+				phone: newPhone,
+				email: newEmail
+			});
+		},
+		[newName, newAddress, newPhone, newEmail] //paso las props como arreglos
+	);
+
+	return (
+		<div className="container">
+			<div>
+				<h1 className="text-center mt-5">Add a new contact</h1>
+				<form>
+					<div className="form-group">
+						<label>Full Name</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Full Name"
+							value={newName} // el valor escrito en new name
+							onChange={event => setnewName(event.target.value)} // se secuestra en el arreglo setnewName
+						/>
+					</div>
+					<div className="form-group">
+						<label>Email</label>
+						<input
+							type="email"
+							className="form-control"
+							placeholder="Enter email"
+							value={newEmail} // el valor escrito en new email
+							onChange={event => setnewEmail(event.target.value)} // se secuestra en el arreglo setnewEmail
+						/>
+					</div>
+					<div className="form-group">
+						<label>Phone</label>
+						<input
+							type="phone"
+							className="form-control"
+							placeholder="Enter phone"
+							value={newPhone} // el valor escrito en new phone
+							onChange={event => setnewPhone(event.target.value)} // se secuestra en el arreglo setnewPhone
+						/>
+					</div>
+					<div className="form-group">
+						<label>Address</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter address"
+							value={newAddress} // el valor escrito en new address
+							onChange={event => setnewAddres(event.target.value)} // se secuestra en el arreglo setnewAddress
+						/>
+					</div>
+					<button
+						type="button"
+						className="btn btn-primary form-control"
+						onClick={() => {
+							actions.añadirContactos(arrayContacto); // al precionar el boton de guardar uso la accion de añadirContactos y hago post con el arrayContact hacia la url
+						}}>
+						Guardar
+					</button>
+					<Link className="mt-3 w-100 text-center" to="/">
+						O volver a contactos
+					</Link>
+				</form>
+			</div>
+		</div>
+	);
+};
